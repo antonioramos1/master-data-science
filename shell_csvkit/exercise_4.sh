d922765606594f52a7ce4a0f4b342a1a0c158e66
Antonio <antonioramosglz@gmail.com>
2018-04-03 15:32:00 +0200

    correction exercise 4

--- shell_csvkit/exercise_4.sh
+++ shell_csvkit/exercise_4.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-cat /home/dsc/Data/opentraveldata/optd_aircraft.csv| csvgrep -d '^' -c 'manufacturer' -m 'Airbus' | >> airbus.csv
+cat /home/dsc/Data/opentraveldata/optd_aircraft.csv| csvgrep -d '^' -c 'manufacturer' -m 'Airbus' | csvformat -D "|" >> airbus.csv
+
