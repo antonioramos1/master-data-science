b4179cb8efd9f867804ad294e0101f38483f5e4e
Antonio <antonioramosglz@gmail.com>
2018-04-03 18:18:33 +0200

    corrected exercise 1

--- shell_scripting/exercise_1.sh
+++ shell_scripting/exercise_1.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 FILE_NAME="$1"
-csvcut -d '^' -c 7 /home/dsc/Data/opentraveldata/$FILE_NAME | csvstat --max
+
+csvsort -d '^' -r -c 7 /home/dsc/Data/opentraveldata/$FILE_NAME | csvgrep -c 7 -r "\S" | csvcut -c 3 | head -2 | tail -1
+
